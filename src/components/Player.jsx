03768caab019ffb98eda8a604621d85936b7070f
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-03-18 17:49:45 +0100

    sostituito metodo precedente con l'utilizzo dei Refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
+import { useState, useRef } from "react"; 
 
 export default function Player() {
+  const playerName = useRef();          // permette un codice più pulito e snello e per casi d'uso simili a questo
+
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
   
 
-  const handleChange = (event) => {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  // const handleChange = (event) => {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
 
   const handleClick = () => {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value); 
+
+    // setSubmitted(true);
   }
 
   
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>
+        Welcome {enteredPlayerName ?? "unknown entity"}
+        {/* {submitted ? enteredPlayerName : "unknown entity"} */}
+        </h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input 
+        ref={playerName} 
+        type="text" 
+        // onChange={handleChange} 
+        // value={enteredPlayerName} 
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
